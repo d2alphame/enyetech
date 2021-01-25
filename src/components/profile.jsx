5a95ac61b909165db33aab870421d1e1159a6c95
Deji Adegbite <d2alphame@yahoo.com>
2021-01-25 10:47:55 +0100

    feature(profile-list): Add profile and profiles to components

---
+++ src/components/profile.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+	state = {  }
+	render() { 
+		return (<h1>Hello World</h1>);
+	}
+}
+ 
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+// import Profile from './components/profile'
 
 ReactDOM.render(
   <React.StrictMode>
