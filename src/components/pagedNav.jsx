09f830ce78767c4efc01f183b52555efa273fb03
Deji Adegbite <d2alphame@yahoo.com>
2021-01-26 17:54:00 +0100

    feat(profiles.jsx): Add sequential navigation of paged profile list

--- src/components/pagedNav.jsx
+++ src/components/pagedNav.jsx
@@ -5,12 +5,12 @@ class PagedNav extends Component {
 	render() { 
 		return ( 
 			<nav aria-label="Page navigation example">
-  					<ul class="pagination">
-    				<li class="page-item"><a class="page-link" href="#">First</a></li>
-    				<li class="page-item"><a class="page-link" href="#">Prev</a></li>
-    				<li class="page-item"><a class="page-link" href="#">1/1</a></li>
-    				<li class="page-item"><a class="page-link" href="#">Next</a></li>
-    				<li class="page-item"><a class="page-link" href="#">Last</a></li>
+  				<ul class="pagination">
+    				<li className="page-item"><a className="page-link" href="#">First</a></li>
+    				<li className="page-item" onClick={this.props.prevPageHandler}><a className="page-link" href="#">Prev</a></li>
+    				<li className="page-item"><a className="page-link" href="#">{this.props.currPage}/{this.props.totPages}</a></li>
+    				<li className="page-item" onClick={this.props.nextPageHandler}><a className="page-link" href="#">Next</a></li>
+    				<li className="page-item"><a className="page-link" href="#">Last</a></li>
   				</ul>
 			</nav>
 		 );
--- src/components/profiles.jsx
+++ src/components/profiles.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PagedNav from './pagedNav'
 
 class Profiles extends Component {
 
@@ -16,13 +17,15 @@ class Profiles extends Component {
 			.then(res => res.json())
 			.then(
 				(result) => {
+					let totalPages = Math.floor(result.size / 20) + (result.size % 20 ? 1 : 0)
 					this.setState({
 						loaded: true,
 						profiles: result.records.profiles,
 						status: result.status,
 						size: result.size,
 						currentPage: 1,
-						pageLimit: 20
+						pageLimit: 20,
+						totalPages: totalPages
 					})
 				},
 				(error) => {
@@ -35,7 +38,7 @@ class Profiles extends Component {
 	}
 
 	render() { 
-		const { error, loaded} = this.state
+		const { error, loaded, currentPage, pageLimit, profiles} = this.state
 		if(error) {
 			return ( <div>Error: { error.message } </div> )
 		}
@@ -43,22 +46,30 @@ class Profiles extends Component {
 			return ( <div>Loading...</div> )
 		}
 
-		let displayProfiles = this.gotoPage(1, 20)
+		let displayProfiles = this.gotoPage(currentPage, pageLimit, profiles)
 		return ( 
 			<div>
+				<PagedNav currPage={currentPage} totPages={this.state.totalPages} nextPageHandler={this.gotoNextPage} prevPageHandler={this.gotoPrevPage} />
 				{ displayProfiles.map(profile => 
 					<div className="card my-5" key={profile.Email}>
 						<div className="card-header">
-							{profile.FirstName + " " + profile.LastName}
+							<b><i>{profile.FirstName + " " + profile.LastName}</i></b><br />
+							<i>{profile.Email}</i><br />
+							<i>{profile.PhoneNumber}</i><br />
+							<i>{profile.DomainName}</i><br />
 						</div>
 						<div className="card-body">
 							<h5 className="card-title">{profile.UserName}</h5>
-							<p className="font-italic">{profile.Email}</p>
-							<a href={profile.URL} target="_blank" rel="noopener noreferrer" className="card-link" >{profile.URL}</a>
+							Credit Card Number: {profile.CreditCardNumber.substring(0, 4)}***<br />
+							Credit Card Type: {profile.CreditCardType}<br />
+							Payment Method: {profile.PaymentMethod}<br />
+							<a href={profile.URL} target="_blank" rel="noopener noreferrer" className="card-link" >{profile.URL}</a><br />
+							<p></p>
 						</div>
 						<div className="card-footer text-muted">
     						Last logged in on {profile.LastLogin} <br />
-							From {profile.Longitude}, {profile.Latitude}
+							From {profile.Longitude}, {profile.Latitude} <br />
+							With {profile.MacAddress}
   						</div>
 					</div>
 				)}
@@ -67,18 +78,43 @@ class Profiles extends Component {
 	}
 
 	// Returns an array which contains only the profiles for the given page
-	gotoPage = (n, pageLimit) => {
-		const startIndex = pageLimit * (n - 1)
-		const lastIndex = startIndex + pageLimit
-		return this.state.profiles.slice(startIndex, lastIndex)
+	// n is the page number, count is the number of items per page and arr
+	// is the array to page
+	gotoPage = (n, count, arr) => {
+
+		let len = arr.length
+
+		if(n <= 0) { n = 1 }					// We can't go to page 0, can we :D so just go to page 1 instead
+		let startIndex = count * (n - 1)
+
+		// Need to avoid ArrayIndexOutOfBounds
+		if(startIndex >= len) {
+
+			// Rather than ArrayIndexOutOfBounds, just return the last page
+			startIndex = len - (len % count)
+		}
+
+		const lastIndex = startIndex + count
+		return arr.slice(startIndex, lastIndex)
 	}
 
-	gotoNextPage() {
+	
 
+	gotoNextPage = () => {
+		let { currentPage, totalPages } = this.state
+		if(currentPage < totalPages) {
+			currentPage++
+			this.setState({currentPage: currentPage})
+		}
 	}
 
-	gotoPrevPage() {
 
+	gotoPrevPage = () => {
+		let { currentPage } = this.state
+		if(currentPage > 1) {
+			currentPage--
+			this.setState({currentPage: currentPage})
+		}
 	}
 }
  
